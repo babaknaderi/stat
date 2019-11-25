ba5e6f435436037c8b0466c5f8e19a0526b0bd19
Oliver <o.cheung@protonmail.com>
2019-11-25 23:37:26 +0100

    Updated Task3+4+html files

--- W2/group_matlab/Task_03.m
+++ W2/group_matlab/Task_03.m
@@ -1,4 +1,4 @@
-T = readtable("Speech_quality_crowdsourcing_dataset.xlsx","Sheet","CS participants");
+T = readtable("..\..\Speech_quality_crowdsourcing_dataset.xlsx","Sheet","CS participants");
 this_year = year(datetime('now'));
 years = T.BirthYear';
 age = [];
@@ -6,36 +6,38 @@ age = [];
 for i=1 : length(years)
  years(i)= this_year-years(i);
 end
-
+%rename for easier usage later on
 ages = years;
+
 %x-axsis range
-           % bis 18,  18-25,26-36,36-50  
-ranges = [    18,     26,   36,    51 inf];
-            % 0     1       2     3      4
-[bincounts,ind] = histcounts(ages,ranges)
-figure(1)
+           % 18-25,  26-36,36-50, 50+  
+ranges = [    18,     26  ,   36,    51 inf];
+[bincounts,ind] = histcounts(ages,ranges);
+figure(1);
 
 %label names
-binranges_names = []
+binranges_names = [];
 for i=1: length(ranges)-1
     %define lower and upper ranges [18-25], [25-35] ... 
-    str=ranges(i)+"-"+num2str(ranges(i+1)-1)
-    binranges_names{i} = str
+    str=ranges(i)+"-"+num2str(ranges(i+1)-1);
+    binranges_names{i} = str;
 end
 
 %last lable should not be [50 - inf] but 50>
 binranges_names{end}=sprintf('>%d',ranges(end-1));
 %problem: histgram only takes concrete values for x-axis, not ranges (if ranges, than the bar
 %will be very wide (depends on the range). To have same bar width we only set the middle value) 
-centers=ranges(1:(end-1))+diff(ranges(1:2));
-centers=[1,2,3,4]
+centers=[1,2,3,4];
 %plote
-bar(centers,bincounts)
+bar(centers,bincounts);
 %set x axis to center values
-xticks(centers)
+xticks(centers);
 
 %overwrite the x-axis values to range names
-set(gca,'XTickLabels',binranges_names)
+set(gca,'XTickLabels',binranges_names);
 %rotate label for nicer view
-set(gca,'XTickLabelRotation',60)
+set(gca,'XTickLabelRotation',60);
+title('Age Distribution Histogramm');
+xlabel('Age Ranges') 
+ylabel('Amount') 
 
--- W2/group_matlab/Task_04.m
+++ W2/group_matlab/Task_04.m
@@ -1,27 +1,52 @@
-T = readtable("Gaming_Video_Quality_dataset.xlsx");
+T = readtable("..\..\Gaming_Video_Quality_dataset.xlsx");
 t_game4_condition26 = T(T.Condition == 26 & T.Game == "Game4" ,:);
 
-%QQ-Plot
-figure
-qqplot(t_game4_condition26.VQ');
-title('QQ Plot of Video Quality versus Standard Normal');
-ylabel('VQ');
-
-%Kolmogorov-Smirnov
-ks_std = std(t_game4_condition26.VQ)
-ks_mean = mean(t_game4_condition26.VQ)
-z_values = (t_game4_condition26.VQ-ks_mean )/ks_std
-%if 1 = no normal distribution, if 0 = normal distribution
-h = kstest(z_values)
-%plot for Kolmogorov-Smirnov
+%ND
+ks_std = std(t_game4_condition26.VQ);
+ks_mean = mean(t_game4_condition26.VQ);
+z_values = (t_game4_condition26.VQ-ks_mean )/ks_std;
+%plot for cumulate distribution
+subplot(2,1,1);
 cdfplot(z_values)
 hold on
 x_values = linspace(min(z_values),max(z_values));
 %values and color of nd line
 plot(x_values,normcdf(x_values,0,1),'r-')
-title('Cumulative Distribution Functions')
-legend('Empirical CDF','Standard Normal CDF','Location','best')
+title('Cumulative Distribution Functions');
+legend('Empirical CDF','Standard Normal CDF','Location','best');
 
+%Kolmogorov-Smirnov
+%if 1 = no normal distribution, if 0 = normal distribution
+%significance < 0,05,               significance >= 0,05: 
+[h_ks,p_ks_value] = kstest(z_values);
+if(h_ks==1)
+    fprintf("Video Quality is not normal distributed(Kolmogrov-Smirnov-Test) with significant level:%d\n", p_ks_value)
+else
+    fprintf("Video Quality is normal distributed(Kolmogrov-Smirnov-Test) with significant level:%d\n",p_ks_value)
+end
 
 %Shapiro-Wilk 
-[H, pValue, SWstatistic] = swtest(t_game4_condition26.VQ, 0.05)
+% performs the Shapiro-Wilk test to determine if the null hypothesis of composite
+% normality is a reasonable assumption of a given distribution. 
+%   The Shapiro-Wilk and Shapiro-Francia null hypothesis is: 
+%   "X is normal with unspecified mean and variance."
+% - pVAlueis the p-value, or the probability of observing the given
+%   result by chance given that the null hypothesis is true
+% - H
+%     H = 0 => Do not reject the null hypothesis at significance level ALPHA.
+%     H = 1 => Reject the null hypothesis at significance level ALPHA.
+%  if 1 = no normal distribution, if 0 = normal distribution
+%  significance < 0,05,               significance >= 0,05: 
+[h_sw, p_sw_value, SWstatistic] = swtest(t_game4_condition26.VQ, 0.05);
+
+if(h_sw==1)
+    fprintf("Video Quality is not normal distributed(Shapiro-Wilk) with significant level: %d\n", p_sw_value)
+else
+    fprintf("Video Quality is normal distributed(Shapiro-Wilk) with significant level:%d\n",p_sw_value)
+end
+
+
+%QQ-Plot
+subplot(2,1,2);
+qqplot(t_game4_condition26.VQ');
+title('QQ Plotof Video Quality versus Standard Normal');
